0636d64377872574f105a36fb5fa92ef84a8536e
Hanny Paz <hwpazz@gmail.com>
2025-04-14 23:48:57 -0500

    Realizando algunos cambios y agregando algunos Hooks de react

--- src/Components/CalculosPesados.jsx
+++ src/Components/CalculosPesados.jsx
@@ -1,16 +1,23 @@
+import { useState } from "react";
+
 const getCalculo = (listaNumeros) => {
   console.log("calculando");
   return listaNumeros.reduce((a, b) => a * b);
 };
 
-export const CalculosPesados = () => {
-  const listaNumeros = [1, 2, 3, 4, 5];
+const agregarNumero = () => {
+  listaNumeros[length - 1] + 1;
+};
 
+export const CalculosPesados = () => {
+  const [listaNumeros, setListaNumeros] = useState([1, 2, 3, 4, 5, 6, 7, 8]);
+  const [show, setShow] = useState(true);
   return (
     <>
       <h2>Calculo: </h2>
       <p>{getCalculo(listaNumeros)}</p>
-      <button onClick={(setShow) => !show}>{show ? "show" : "Hide"}</button>
+      <button onClick={() => setShow(!show)}>{show ? "show" : "Hide"}</button>
+      <button onClick={() => agregarNumero()}>Agregar Número</button>
     </>
   );
 };
