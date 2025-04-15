7cfa84c8c9560d11273e5e16d81d5d0a7a34c676
Hanny Paz <hwpazz@gmail.com>
2025-04-15 18:11:41 -0500

    Por ultimo se agrego bootstrap para los botones

--- src/Components/CalculosPesados.jsx
+++ src/Components/CalculosPesados.jsx
@@ -1,23 +1,34 @@
+import { useMemo } from "react";
 import { useState } from "react";
-
-const getCalculo = (listaNumeros) => {
-  console.log("calculando");
-  return listaNumeros.reduce((a, b) => a * b);
-};
-
-const agregarNumero = () => {
-  listaNumeros[length - 1] + 1;
-};
 
 export const CalculosPesados = () => {
   const [listaNumeros, setListaNumeros] = useState([1, 2, 3, 4, 5, 6, 7, 8]);
   const [show, setShow] = useState(true);
+
+  const getCalculo = (listaNumeros) =>
+    useMemo(() => {
+      console.log("Calculando");
+      return listaNumeros.reduce((a, b) => a * b);
+    }, [listaNumeros]);
+
+  const agregarNumero = () => {
+    setListaNumeros([
+      ...listaNumeros,
+      listaNumeros[listaNumeros.length - 1] + 1,
+    ]);
+    console.log(listaNumeros);
+  };
+
   return (
     <>
       <h2>Calculo: </h2>
       <p>{getCalculo(listaNumeros)}</p>
-      <button onClick={() => setShow(!show)}>{show ? "show" : "Hide"}</button>
-      <button onClick={() => agregarNumero()}>Agregar Número</button>
+      <button className="btn btn-primary" onClick={() => setShow(!show)}>
+        {show ? "show" : "Hide"}
+      </button>
+      <button className="btn btn-primary" onClick={() => agregarNumero()}>
+        Agregar Número
+      </button>
     </>
   );
 };
